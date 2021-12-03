86ee6ff3ac61ec086cf852d79a58e80efd2d34c2
anitapanczel <anita@panczel.hu>
2021-12-03 08:45:51 +0100

    insert files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# w5-js-practices
+#Santa Challenge
+
+INSTRUCTIONS
+The code we received from the future asking us to fix SantaBot© is completely unrecognizable. But with time, we have been able to reverse engineer some of their requirements! Our team has so far deciphered one of the four main routines of SantaBot©, and will be able to decipher one more main routine every next week. With your help, we can still reprogram the code in time and send it back before Christmas eve!
+
+You are free to decide how to implement the main routines of SantaBot©. However, the citizens of the year 3000 would greatly appreciate it if they would receive a cleaner code free of bugs that they can maintain by themselves afterwards. Therefore, we definitely encourage you to:
+
+Use the programming language you feel the most comfortable with.
+Apply the principles of Clean Code as much as possible.
+Apply the KISS, DRY and SLAP principles as much as possible.
+Apply the Coherence principle, keeping the code organized in files and packages accordingly.
+Provide tests for each piece of logic that you write.
+ 
+If you use an object oriented programming language, apply the SOLID principles as well.
+
+ 
+The solutions of the challenges are not validated, you do it for your own entertainment & improvement.
--- script.js
+++ script.js
@@ -2,4 +2,75 @@ window.addEventListener("load", init);
 
 function init() {
     console.log("hello");
+    document.getElementById("root").insertAdjacentHTML("beforeend", createCards(3));
+}
+
+function createCards(howMany) {
+    let createTags = "";
+    for (let i = 0; i < howMany; i++) {
+        createTags += `
+        <div class="tags">
+            <div class="tag-container">
+                <div class="tag">
+                    <div class="tag-side tag-1-side">
+                        <div class="tag-1-top"></div>
+                        <div class="tag-text tag-1-text cardName">
+                            Here goes<br>the name
+                            <div class="rule-shape rule-red">&starf;</div>
+                        </div>
+                    </div>
+                    <div class="tag-side tag-1-side is-back">
+                        <div class="tag-1-top"></div>
+                        <div class="tag-text tag-1-text surprise">
+                            Here goes<br>the surprise
+                            <div class="rule-shape">&starf;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tag-container">
+                <div class="tag">
+                    <div class="tag-side tag-2-side">
+                        <div class="tag-text tag-2-text cardName">
+                            Happy<br />Holidays
+                            <div class="rule-diagonal"></div>
+                        </div>
+                    </div>
+        
+                    <div class="tag-side tag-2-side is-back">
+                        <div class="tag-text tag-2-text surprise">
+                            May your ev'ry wish come true
+                            <div class="rule-diagonal"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tag-container">
+                <div class="tag">
+                    <div class="tag-side tag-3-side">
+                        <div class="tag-text tag-3-text cardName">
+                            Feliz<br />Navidad
+                            <div class="rule-shape">&#10052;</div>
+                        </div>
+                    </div>
+                    <div class="tag-side tag-3-side is-back">
+                        <div class="tag-text tag-3-text surprise">
+                            Prospero Año Nuevo
+                            <div class="rule-shape">&#10052;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+
+    }
+
+    return createTags;
+
+}
+
+function cardNames() {
+    for (const name of profiles) {
+
+    }
 }
