1bba5f20bfc20143daaa7e8fa6afb8edfc172224
anitapanczel <anita@panczel.hu>
2021-12-06 08:45:07 +0100

    done

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ window.addEventListener("load", init);
 
 function init() {
     console.log("hello");
-    document.getElementById("root").insertAdjacentHTML("beforeend", createCards(3));
+    document.getElementById("root").insertAdjacentHTML("beforeend", createCards());
 }
 
-function createCards(howMany) {
+function createCards() {
     let createTags = "";
-    for (let i = 0; i < howMany; i++) {
+
+    for (let index = 0; index < 48; index++) {
+        let present1 = profiles[index].present.split(",");
+        let present2 = profiles[index + 1].present.split(",");
+        let present3 = profiles[index + 2].present.split(",");
+
         createTags += `
         <div class="tags">
             <div class="tag-container">
@@ -15,54 +20,57 @@ function createCards(howMany) {
                     <div class="tag-side tag-1-side">
                         <div class="tag-1-top"></div>
                         <div class="tag-text tag-1-text cardName">
-                            Here goes<br>the name
+                            ${profiles[index].name}
                             <div class="rule-shape rule-red">&starf;</div>
-                        </div>
-                    </div>
-                    <div class="tag-side tag-1-side is-back">
-                        <div class="tag-1-top"></div>
-                        <div class="tag-text tag-1-text surprise">
-                            Here goes<br>the surprise
+                            </div>
+                            </div>
+                            <div class="tag-side tag-1-side is-back">
+                            <div class="tag-1-top"></div>
+                            <div class="tag-text tag-1-text surprise">
+                            ${present1[0]}
                             <div class="rule-shape">&starf;</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="tag-container">
-                <div class="tag">
-                    <div class="tag-side tag-2-side">
-                        <div class="tag-text tag-2-text cardName">
-                            Happy<br />Holidays
-                            <div class="rule-diagonal"></div>
-                        </div>
-                    </div>
-        
-                    <div class="tag-side tag-2-side is-back">
-                        <div class="tag-text tag-2-text surprise">
-                            May your ev'ry wish come true
-                            <div class="rule-diagonal"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tag-container">
-                <div class="tag">
-                    <div class="tag-side tag-3-side">
-                        <div class="tag-text tag-3-text cardName">
-                            Feliz<br />Navidad
-                            <div class="rule-shape">&#10052;</div>
-                        </div>
-                    </div>
-                    <div class="tag-side tag-3-side is-back">
-                        <div class="tag-text tag-3-text surprise">
-                            Prospero Año Nuevo
-                            <div class="rule-shape">&#10052;</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`
 
+            <div class="tag-container">
+            <div class="tag">
+                <div class="tag-side tag-2-side">
+                    <div class="tag-text tag-2-text cardName">
+                    ${profiles[index+1].name}
+                        <div class="rule-diagonal"></div>
+                    </div>
+                </div>
+    
+                <div class="tag-side tag-2-side is-back">
+                    <div class="tag-text tag-2-text surprise">
+                    ${present2[0]}
+                        <div class="rule-diagonal"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tag-container">
+            <div class="tag">
+                <div class="tag-side tag-3-side">
+                    <div class="tag-text tag-3-text cardName">
+                        ${profiles[index+2].name}
+                        <div class="rule-shape">&#10052;</div>
+                    </div>
+                </div>
+                <div class="tag-side tag-3-side is-back">
+                    <div class="tag-text tag-3-text surprise">
+                    ${present3[0]}
+                        <div class="rule-shape">&#10052;</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`
+        index++;
+        index++;
     }
 
     return createTags;
